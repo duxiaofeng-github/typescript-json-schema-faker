f505babc6243ffd10773151ef4178bb375bc9f22
duxiaofeng <csdxf297951292@gmail.com>
2018-05-30 12:47:08 +0800

    update .d.ts for supporting reference schema
    Signed-off-by: duxiaofeng <csdxf297951292@gmail.com>

--- index.d.ts
+++ index.d.ts
@@ -2,57 +2,65 @@
  * JSF basic schema extension
  */
 interface IGeneratorSchema {
-    faker?: any;
-    'x-faker'?: any;
-    chance?: any;
-    'x-chance'?: any;
-    casual?: any;
-    'x-casual'?: any;
+  faker?: any;
+  "x-faker"?: any;
+  chance?: any;
+  "x-chance"?: any;
+  casual?: any;
+  "x-casual"?: any;
 }
 
 interface ICommonSchema {
-    type?: ISchemaType;
+  type?: ISchemaType;
 }
 
 interface IStringSchema extends IGeneratorSchema, ICommonSchema {
-    format?: string;
-    pattern?: RegExp;
-    minLength?: number;
-    maxLength?: number;
+  format?: string;
+  pattern?: RegExp;
+  minLength?: number;
+  maxLength?: number;
 }
 
 interface INumberSchema extends IGeneratorSchema, ICommonSchema {
-    multipleOf?: number;
-    minimum?: number;
-    maximum?: number;
-    exclusiveMinimum?: boolean;
-    exclusiveMaximum?: boolean;
+  multipleOf?: number;
+  minimum?: number;
+  maximum?: number;
+  exclusiveMinimum?: boolean;
+  exclusiveMaximum?: boolean;
 }
 
 interface IArraySchema extends IGeneratorSchema, ICommonSchema {
-    items?: IObjectSchema|IObjectSchema[];
-    additionalItems?: boolean|IObjectSchema;
-    minItems?: number;
-    maxItems?: number;
-    uniqueItems?: boolean;
+  items?: IObjectSchema | IObjectSchema[];
+  additionalItems?: boolean | IObjectSchema;
+  minItems?: number;
+  maxItems?: number;
+  uniqueItems?: boolean;
 }
 
 interface IPropertySchema {
-    [property: string]: IObjectSchema|IArraySchema|INumberSchema|IStringSchema;
+  [property: string]:
+    | IObjectSchema
+    | IArraySchema
+    | INumberSchema
+    | IStringSchema;
 }
 
 interface IPatternPropertiesSchema {
-    [property: string]: {};
+  [property: string]: {};
 }
 
 interface IObjectSchema extends IGeneratorSchema, ICommonSchema {
-    properties?: IPropertySchema;
-    patternProperties?: IPropertySchema; // RegExp should be the index of this structure (see "5.4.4.1. Valid values" in http://json-schema.org/latest/json-schema-validation.html), but RegExp-key-based maps are unsupported in TypeScript
-    additionalProperties?: boolean;
-    required?: string[];
-    minProperties?: number;
-    maxProperties?: number;
-    enum?: any[];
+  properties?: IPropertySchema;
+  patternProperties?: IPropertySchema; // RegExp should be the index of this structure (see "5.4.4.1. Valid values" in http://json-schema.org/latest/json-schema-validation.html), but RegExp-key-based maps are unsupported in TypeScript
+  additionalProperties?: boolean;
+  required?: string[];
+  minProperties?: number;
+  maxProperties?: number;
+  enum?: any[];
+}
+
+interface IReferenceSchema {
+  $ref?: string;
 }
 
 type SchemaPath = string[];
@@ -63,15 +71,20 @@ type StackTrace = string[];
  * This interface is used to check consistency between type generators (string, boolean, array, etc.)
  */
 interface FTypeGenerator {
-    (value?: IGeneratorSchema, path?: SchemaPath, resolve?: Function, traverseCallback?: Function): any;
+  (
+    value?: IGeneratorSchema,
+    path?: SchemaPath,
+    resolve?: Function,
+    traverseCallback?: Function
+  ): any;
 }
 
 interface IStringMap {
-    [format: string]: string;
+  [format: string]: string;
 }
 
 interface TypescriptJsonSchemaFakerStatic {
-    (schema: JsonSchema, refs?: any)
+  (schema: JsonSchema, refs?: any);
 }
 
 /**
@@ -79,18 +92,30 @@ interface TypescriptJsonSchemaFakerStatic {
  *
  * fetched from https://github.com/DefinitelyTyped/DefinitelyTyped/blob/master/tv4/tv4.d.ts
  */
-export interface JsonSchema extends IGeneratorSchema, ICommonSchema, IObjectSchema, IArraySchema, INumberSchema, IStringSchema {
-    [key: string]: any;
-    title?: string;
-    description?: string;
-    id?: string;
-    $schema?: string;
-    definitions?: any;
-    default?: any;
+export interface JsonSchema
+  extends IGeneratorSchema,
+    ICommonSchema,
+    IObjectSchema,
+    IArraySchema,
+    INumberSchema,
+    IStringSchema,
+    IReferenceSchema {
+  [key: string]: any;
+  title?: string;
+  description?: string;
+  id?: string;
+  $schema?: string;
+  definitions?: any;
+  default?: any;
 }
 
-export type ISchemaType = 'string' | 'integer' | 'number' | 'object' | 'array' | 'boolean';
+export type ISchemaType =
+  | "string"
+  | "integer"
+  | "number"
+  | "object"
+  | "array"
+  | "boolean";
 
-export function fake(schema: JsonSchema, refs?: any): any;
+export function fake<T>(schema: JsonSchema, refs?: JsonSchema[]): T;
 export default fake;
-
